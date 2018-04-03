4353174513a5295cada61edf2ef4344983dc16ec
Fabian Tjoe-A-On <fabiantjoeaon@gmail.com>
2018-04-03 12:49:15 +0200

    .gitkeep

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require("express");
-var http = require("http").Server(express);
-var io = require("socket.io")(http);
+const express = require('express');
+const http = require('http').Server(express);
+const io = require('socket.io')(http);
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.get('/', (req, res) => {
+    res.send('Hello world');
 });
 
 module.exports = app;
