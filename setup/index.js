bb70a2cfad7b92d9a76be6e9a4af754f76e31f5e
Fabian Tjoe-A-On <fabiantjoeaon@gmail.com>
2018-04-04 15:13:25 +0200

    --

--- setup/index.js
+++ setup/index.js
@@ -1,6 +1,8 @@
 const app = require('./app');
 const io = require('socket.io');
 
+const { updatePackageStatus } = require('../services/packageDeliveryService');
+
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
     console.log(`Express running → PORT ${server.address().port}`);
@@ -10,7 +12,7 @@ io.listen(server);
 io.on('connection', socket => {
     console.log('client connected');
     socket.on('package:change-home-notification', data => {
-        console.log(data);
+        updatePackageStatus(data);
     });
 });
 
