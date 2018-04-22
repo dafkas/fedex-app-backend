f4bb1bc90a6ba975f23c167f1486f5d968e847a3
Fabian Tjoe-A-On <fabiantjoeaon@gmail.com>
2018-04-22 15:54:07 +0200

    socket http

--- setup/index.js
+++ setup/index.js
@@ -1,5 +1,6 @@
 const app = require('./app');
-const io = require('socket.io');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 const { updatePackageStatus } = require('../services/packageDeliveryService');
 
@@ -8,7 +9,6 @@ const server = app.listen(app.get('port'), () => {
     console.log(`Express running → PORT ${server.address().port}`);
 });
 
-io.listen(server);
 io.on('connection', socket => {
     console.log('client connected');
     socket.on('package:change-home-notification', data => {
